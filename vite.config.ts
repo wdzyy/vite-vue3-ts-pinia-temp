a1f765eb7242addbac151e8912665d215bfca2a8
QJia <qianjia@huansi.net>
2022-07-14 18:05:51 +0800

    🎉init: init

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+  server: {
+    port: 8080, //启动端口
+    hmr: {
+      host: "127.0.0.1",
+      port: 8080,
+    },
+    // 设置 https 代理
+    proxy: {
+      "/api": {
+        target: "your https address",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
