f6b364dc82c4d12469c90c653330b3090df06e2a
QJia <qianjia@huansi.net>
2022-07-18 09:42:09 +0800

    🔩chore: 基本配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
-    minify: "terser",
+    minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
@@ -23,15 +23,15 @@ export default defineConfig({
   server: {
     port: 8080, //启动端口
     hmr: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 8080,
     },
     // 设置 https 代理
     proxy: {
-      "/api": {
-        target: "your https address",
+      '/api': {
+        target: 'your https address',
         changeOrigin: true,
-        rewrite: (path: string) => path.replace(/^\/api/, ""),
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
       },
     },
   },
