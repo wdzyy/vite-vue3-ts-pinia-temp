cb67c3a6043ee79338063951f5af4e5aec980912
QJia <qianjia@huansi.net>
2022-07-19 16:05:34 +0800

    chore(custom): electron基础模板

--- vite.config.ts
+++ vite.config.ts
@@ -1,70 +1,64 @@
-import type { UserConfig, ConfigEnv } from 'vite';
-import { resolve } from 'path';
-import { loadEnv } from 'vite';
-import { createProxy } from './build/vite/proxy';
-import { wrapperEnv } from './build/utils';
-import { createVitePlugins } from './build/vite/plugin';
+import { rmSync } from 'fs';
+import { join } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-plugin-electron';
+import pkg from './package.json';
 
-function pathResolve(dir: string) {
-  return resolve(process.cwd(), '.', dir);
-}
+rmSync('dist', { recursive: true, force: true }); // v14.14.0
 
 // https://vitejs.dev/config/
-export default ({ command, mode }: ConfigEnv): UserConfig => {
-  const root = process.cwd();
-
-  const env = loadEnv(mode, root);
-
-  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
-  const viteEnv = wrapperEnv(env);
-
-  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
-
-  const isBuild = command === 'build';
-  return {
-    base: VITE_PUBLIC_PATH,
-    root,
-    resolve: {
-      alias: [
-        {
-          find: /\/@\//,
-          replacement: pathResolve('src') + '/',
-        },
-        // /#/xxxx => types/xxxx
-        {
-          find: /\/#\//,
-          replacement: pathResolve('types') + '/',
-        },
-      ],
-    },
-    build: {
-      target: 'es2015',
-      //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
-      assetsInlineLimit: 4096,
-      //启用/禁用 CSS 代码拆分
-      cssCodeSplit: true,
-      //构建后是否生成 source map 文件
-      sourcemap: false,
-      //chunk 大小警告的限制
-      chunkSizeWarningLimit: 2000,
-      //关闭brotliSize显示可以稍微减少包装时间
-      brotliSize: false,
-      minify: 'terser',
-      terserOptions: {
-        compress: {
-          keep_infinity: true,
-          drop_console: VITE_DROP_CONSOLE,
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            outDir: 'dist/electron/main',
+          },
         },
       },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: join(__dirname, 'electron/preload/index.ts'),
+        },
+        vite: {
+          build: {
+            // For debug
+            sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
+          },
+        },
+      },
+      // Enables use of Node.js API in the Renderer-process
+      renderer: {},
+    }),
+  ],
+  build: {
+    target: 'es2015',
+    //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
+    assetsInlineLimit: 4096,
+    //启用/禁用 CSS 代码拆分
+    cssCodeSplit: true,
+    //构建后是否生成 source map 文件
+    sourcemap: false,
+    //chunk 大小警告的限制
+    chunkSizeWarningLimit: 2000,
+    //关闭brotliSize显示可以稍微减少包装时间
+    brotliSize: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        drop_console: true,
+      },
     },
-    server: {
-      port: VITE_PORT, //启动端口
-      open: true,
-      host: true,
-      // 设置 https 代理
-      proxy: createProxy(VITE_PROXY),
-    },
-    // The vite plugin used by the project. The quantity is large, so it is separately extracted and managed
-    plugins: createVitePlugins(viteEnv, isBuild),
-  };
-};
+  },
+  server: {
+    host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
+  },
+});
